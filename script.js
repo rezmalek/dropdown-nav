df5faf056afc40b10713c14eaa1bd15c03b14397
RezMalek <rezmalek7@gmail.com>
2020-05-28 10:26:30 -0700

    complete script.js

--- script.js
+++ script.js
@@ -5,10 +5,29 @@ const nav = document.querySelector('.top');
 function handleEnter() {
     this.classList.add('trigger-enter');
     setTimeout(() => this.classList.add('trigger-enter-active'));
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown'); // we need to select dropdown here inside our function beacuse there are three diffrenet dropdwons. 
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    };
+
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+    
+    
 };
 
 function handleLeave() {
     this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
 };
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
